86340851b83abe9ea1749c9ca916f725acb82c16
SoniSingh17 <soni171204s@gmail.com>
2025-06-08 17:11:10 +0530

    login logout completed

--- crm/website/views.py
+++ crm/website/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate , login , logout
+from django.contrib import messages
 
 # Create your views here.
+def home(request):
+    # check if user is loging...
+    if request.method=="POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"Loged in successfully !!")
+            return redirect('home')
+        else:
+            messages.error(request,"Username or password may be incorrect...Try Again")
+            return redirect('home')
+
+    
+
+    else:
+        return render(request,'website/home.html',{})
+
+
+
+
+# logout ...
+def logout_user(request):
+    logout(request)
+    messages.success(request,"Logged out succesfully :)")
+    return redirect('home')
+
+  
