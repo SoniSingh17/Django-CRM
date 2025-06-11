ca6a38b9b9632b8af0109b4ef54417098cfddcbd
SoniSingh17 <soni171204s@gmail.com>
2025-06-11 12:26:16 +0530

    creating registration form and Model for the user(customers)

--- crm/website/models.py
+++ crm/website/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    last_name =  models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=25)
+    state = models.CharField(max_length=30)
+    pincode = models.CharField(max_length=6)
+
+    def __str__(self):
+        return f"First Name : {self.first_name}\n Last Name : {self.last_name}"
+
--- crm/website/views.py
+++ crm/website/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate , login , logout
 from django.contrib import messages
+from . forms import SignUpForm
+from . models import Record
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
     # check if user is loging...
     if request.method=="POST":
         username = request.POST['username']
@@ -21,7 +24,7 @@ def home(request):
     
 
     else:
-        return render(request,'website/home.html',{})
+        return render(request,'website/home.html',{'records':records})
 
 
 
@@ -32,4 +35,26 @@ def logout_user(request):
     messages.success(request,"Logged out succesfully :)")
     return redirect('home')
 
-  
+# Register
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request,username=username,password=password)
+            if user is not None:
+                login(request,user)
+                messages.success(request,'You Have Logged in Successfully !!')
+                return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request,'website/register.html',{'form':form})
+
+
+
+
+
